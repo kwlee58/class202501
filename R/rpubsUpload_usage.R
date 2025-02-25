70506c4738621028d71724786cc23959c30b9bcf
coop711 <coop711@icloud.com>
2025-02-25 21:37:11 +0900

    250225 modified 2

--- R/rpubsUpload_usage.R
+++ R/rpubsUpload_usage.R
@@ -1,3 +1,15 @@
 library(markdown)
-result0 <- rpubsUpload("Red and Black 240226 : Search for the Best First 1 Million Trials", "./R/Red_and_Black_optimum_240226_prototype_v5_0_1.html")
+result0 <- 
+  rpubsUpload("Red and Black 250225 : Search for the Best 1st 1 Million Trials", 
+              "./R/Red_and_Black_prototype_250225_v5_0_1.html")
 result0
+
+## Mac 에서 오류 발생해도 URL 그대로 사용하면 됨.
+
+library(rsconnect)
+result0 <- 
+  rpubsUpload("Red and Black 250225 : Search for the Best First 1 Million Trials", 
+              "./R/Red_and_Black_prototype_250225_v5_0_1.html", 
+              "./R/Red_and_Black_prototype_250225_v5_0_1.Rmd")
+
+## Mac 에서도 잘 작동함. originalDoc 빠뜨리면 오류 발생함.
