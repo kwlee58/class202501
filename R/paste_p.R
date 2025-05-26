a6e1c37ddfa253091fec9ca80f25e4c9d8263008
coop711 <coop711@icloud.com>
2025-05-26 18:24:36 +0900

    Monty modified

--- R/paste_p.R
+++ R/paste_p.R
@@ -1,4 +1,6 @@
 paste_pp <-
-function(tbl) {
-t(matrix(paste0(format(prop.table(tbl) * 100, digits = 2, nsmall = 1), "%"), nrow = dim(tbl)[1], dimnames = dimnames(tbl)))
+function (tbl) 
+{
+    t(matrix(paste0(format(prop.table(tbl) * 100, digits = 2, 
+        nsmall = 1), "%"), nrow = dim(tbl)[1], dimnames = dimnames(tbl)))
 }
